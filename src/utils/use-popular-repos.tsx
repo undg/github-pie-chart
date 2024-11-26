0e01ea0a916d5a2c6cd74268f937e5b4b71b5a1b
undg <und3rdg@gmail.com>
2024-11-26 11:54:41 +0000

    modern syntax for useQuery

--- src/utils/use-popular-repos.tsx
+++ src/utils/use-popular-repos.tsx
@@ -1,13 +1,21 @@
-import { useQuery } from 'react-query';
-import {Endpoints} from '@octokit/types'
+import { useQuery } from "react-query"
+import { Endpoints } from "@octokit/types"
 
-type Repository = Endpoints['GET /search/repositories']['response']['data']['items'][number]
+type Repository =
+	Endpoints["GET /search/repositories"]["response"]["data"]["items"][number]
 
 export function usePopularRepos() {
-  return useQuery('typescriptRepos', async () => {
-    const response = await fetch('https://api.github.com/search/repositories?q=language:typescript&sort=stars&order=desc&per_page=10');
-    if (!response.ok) throw new Error('Network response not ok');
-    const data = await response.json();
-    return data.items as Repository[];
-  });
+	return useQuery({
+		queryKey: 'typescriptRepos',
+		queryFn: async () => {
+			const response = await fetch(
+				"https://api.github.com/search/repositories?q=language:typescript&sort=stars&order=desc&per_page=10"
+			)
+
+			if (!response.ok) throw new Error("Network response not ok")
+
+			const data = await response.json()
+			return data.items as Repository[]
+		},
+	})
 }
